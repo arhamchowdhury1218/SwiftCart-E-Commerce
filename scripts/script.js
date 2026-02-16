866e446a8a4677a370779286b3227d9026d70aa5
ärhamchowdhury1218 <arhamchowdhury1218@gmail.com>
2026-02-16 13:40:47 +0600

    fetched the product data in the api and showing data in the card dynamically

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,64 @@
-console.log("Hello, World! This is a simple JavaScript file.");
+const loadProducts = () => {
+  fetch("https://fakestoreapi.com/products")
+    .then((res) => res.json())
+    .then((data) => displayProducts(data));
+};
+
+const displayProducts = (products) => {
+  const productsContainer = document.getElementById("products-container");
+  productsContainer.innerHTML = "";
+  products.forEach((product) => {
+    const productDiv = document.createElement("div");
+    productDiv.classList.add(
+      "card",
+      "bg-base-100",
+      "w-96",
+      "shadow-sm",
+      "border",
+      "border-gray-200",
+      "mx-auto",
+      "gap-3",
+    );
+    productDiv.innerHTML = `
+    <div class="flex items-center justify-center bg-gray-200 p-10">
+
+      <figure>
+            <img
+              class="h-40 w-40"
+              src="${product.image}"
+              alt="${product.title}"
+            />
+          </figure>
+          </div>
+          <div class="card-body">
+            <div>
+              <div class="card-actions justify-between">
+                <div class="badge badge-soft badge-primary bg-gray-200">
+                    ${product.category}
+                </div>
+                <div class="flex items-center gap-1">
+                  <img class="h-4 w-4" src="/Assets/star.png" alt="Star Logo" />
+                  <p>${product.rating.rate}(${product.rating.count})</p>
+                </div>
+              </div>
+              <div></div>
+            </div>
+
+            <h2 class="card-title">${product.title}</h2>
+            <p class="font-bold text-2xl">$${product.price}</p>
+            <div class="card-actions justify-between mt-4">
+              <button class="btn btn-neutral btn-outline">
+                <i class="fa-regular fa-eye"></i>Detail
+              </button>
+
+              <button class="btn btn-primary">
+                <i class="fa-solid fa-cart-shopping"></i>Buy Now
+              </button>
+            </div>
+          </div>
+    `;
+    productsContainer.appendChild(productDiv);
+  });
+};
+
+loadProducts();
